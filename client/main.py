b18bada34cce6925119fcc84d1f33247dd2faf0b
Salva Corts <salvacorts97@gmail.com>
2021-06-16 08:46:30 +0200

    Ask for models always

--- client/main.py
+++ client/main.py
@@ -1,18 +1,40 @@
 import time
+from typing import Counter
 import grpc
+import argparse
 from protobuf.api_pb2 import ModelResults, Empty
 from protobuf.api_pb2_grpc import APIStub
 
-with grpc.insecure_channel("localhost:10000") as channel:
-    stub = APIStub(channel)
+def ParseArgs() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
 
-    while True:
-        params = stub.GetModelParams(Empty())
-        print(params)
+    parser.add_argument("-s","--server", action="store", dest="server",
+                        type=str, default="localhost:10000",
+                        help="Server address to connect to")
 
-        results = ModelResults()
-        results.model_id = params.model_id
-        results.recall = 0.8
-        _ = stub.ReturnModel(results)
+    return parser.parse_args()
 
-        time.sleep(2)
+
+if __name__ == "__main__":
+    args = ParseArgs()
+
+    print(f"Connecting to {args.server}")
+    with grpc.insecure_channel(args.server) as channel:
+        stub = APIStub(channel)
+
+        while True:
+            try:
+                params = stub.GetModelParams(Empty())
+                print(params)
+
+                results = ModelResults()
+                results.model_id = params.model_id
+                results.recall = 0.8
+                _ = stub.ReturnModel(results)
+
+                time.sleep(2)
+            except Exception as e:
+                #print(e)
+                time.sleep(5)
+
+            
