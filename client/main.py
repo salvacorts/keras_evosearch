8fc917638411b8393e4dbf79c9bf7ef17898012e
angelessoto <angelessope@gmail.com>
2021-06-18 19:59:08 +0200

    change dataset

--- client/main.py
+++ client/main.py
@@ -42,7 +42,7 @@ def CreateModel(params: ModelParameters) -> Sequential:
         if i == 0:
             model.add(Dense(units=layer.num_neurons,
                             activation=activation_func,
-                            input_shape=(9,)))
+                            input_shape=(12,)))
         else:
             model.add(Dense(units=layer.num_neurons,
                             activation=activation_func))
@@ -62,14 +62,13 @@ def CreateModel(params: ModelParameters) -> Sequential:
 if __name__ == "__main__":
     args = ParseArgs()
 
-    df = pd.read_csv('https://raw.githubusercontent.com/jeffheaton/proben1/master/cancer/breast-cancer-wisconsin.data', header=None)
-    df.drop(columns=[0], inplace=True)
-    df.replace('?', np.nan, inplace=True)
-    df.dropna(inplace=True)
-    df[10] = df[10].map(lambda x: 1 if x == 4 else 0)
-
-    X = np.array(df.drop([10], axis=1))
-    y = np.array(df[10])
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/00547/Algerian_forest_fires_dataset_UPDATE.csv',skiprows=[0,124,125,126])
+    df.columns=df.columns.map(lambda x:x.strip())
+    df.drop(columns=['day'], inplace=True)
+    df.dropna(inplace=True)    
+    df['Classes'] = df['Classes'].map(lambda x: 1 if x == 'fire' else 0)
+    X = np.array(df.drop(['Classes'], axis=1))
+    y = np.array(df['Classes'])
 
     scaler = preprocessing.MinMaxScaler()
     X = scaler.fit_transform(X)
